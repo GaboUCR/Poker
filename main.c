7d58f34ff8a58ae1b3ef825d0c8d808896eea691
Gabriel <gabrielgamboav19@gmail.com>
2022-11-20 15:04:07 -0600

    funcion apostar y primer ciclo de juego terminado

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+enum bool {
+    True = 1,
+    False = 0
+};
+
 enum tipo_carta {
     diamantes=0,
     corazones=1,
@@ -38,13 +43,16 @@ typedef struct mazo {
 typedef struct jugador {
     char * nombre;
     carta * mano;
+    enum bool en_ronda;
     int dinero
 } jugador;
 
 typedef struct juego {
     carta *mesa;
+    int apuesta_actual;
     int cantidad_cartas;
-    int apuesta_minima    
+    int apuesta_minima;
+    int jugadores_en_ronda;    
 } juego;
 
 mazo* crear_mazo () {
@@ -155,13 +163,74 @@ void imprimir_mano (carta* mano, int cantidad) {
     printf("\n");
 }
 
+void apuesta (juego* juego, jugador* jugador, int cantidad) {
+
+    printf("Es el turno de %s su dinero actual es: %d\n", jugador -> nombre, jugador -> dinero);
+
+    printf("Las cartas sobre la mesa son: \n");
+    imprimir_mano(juego->mesa, juego->cantidad_cartas);
+
+    printf("Su mano es: \n");
+    imprimir_mano(jugador -> mano, cantidad);
+
+    while (1) {
+
+        int apuesta;
+        printf("¿Cuanto desea apostar? La apuesta mínima es: %d \n para retirarse inserte un -1 \n", juego->apuesta_minima);
+        scanf("%d", &apuesta);
+
+        //pendiente: condiciones de apuesta
+
+        if (apuesta == -1) {
+            jugador -> en_ronda = False;
+            juego -> jugadores_en_ronda -= 1;
+            break;
+        }
+
+        else if (apuesta > jugador -> dinero) {
+            printf(" \n\n\nNo puede apostar dinero que no tiene. su saldo actual es: %d \n", jugador -> dinero);
+        }
+
+        else if (apuesta < juego->apuesta_minima) {
+            printf(" \n\n\nLa apuesta deber ser igual o mayor que %d \n", juego->apuesta_minima);
+        }
+        
+        else {
+            juego -> apuesta_minima = apuesta;
+            juego -> apuesta_actual += apuesta;
+
+            jugador -> dinero -= apuesta;
+            jugador -> en_ronda = True;
+            break;
+        }
+
+    }
+    printf("\n \n \n \n \n");
+}
+
+void cargar_jugadores_en_ronda(jugador* jugadores, int cantidad_jugadores, int* cantidad_jugadores_en_ronda) {
+
+    int j = 0;
+
+    for (int i = 0; i < cantidad_jugadores; i++) {
+
+        if ( (jugadores+i)->dinero > 0 ) {
+            (jugadores + i) -> en_ronda = True;
+            j++;
+        }
+        else {
+            (jugadores + i) -> en_ronda = False;
+        }
+    }
+    *cantidad_jugadores_en_ronda = j;
+}
+
 void jugar () {
     
     int cantidad_jugadores = 2;
     int cantidad_jugadores_eliminados = 0;
 
     jugador* jugadores = malloc(cantidad_jugadores*sizeof(jugador));
-    jugador* jugadores_eliminados = malloc(cantidad_jugadores*sizeof(jugador));
 
     int dinero_inicial = 2000;
 
@@ -169,35 +238,38 @@ void jugar () {
     jugador1.nombre = "Gabp";
     jugador1.dinero = dinero_inicial;
     jugador jugador2;
-    jugador1.nombre = "Gabp";
-    jugador1.dinero = dinero_inicial;
+    jugador2.nombre = "yolo";
+    jugador2.dinero = dinero_inicial;
 
     *jugadores = jugador1;
     *(jugadores + 1) = jugador2;
 
-    juego juego;
-    mazo *mazo = crear_mazo();
 
     while (cantidad_jugadores_eliminados != cantidad_jugadores - 1) {
+        
+        juego juego;
+        juego.jugadores_en_ronda = cantidad_jugadores;
+        mazo *mazo = crear_mazo();
+
         //cartas iniciales sobre la mesa
         juego.mesa = repartir(mazo, 2);
         juego.cantidad_cartas = 2;
+        juego.apuesta_minima = 0;
+        int cartas_jugador = 2;
 
+        cargar_jugadores_en_ronda(jugadores, cantidad_jugadores, &juego.jugadores_en_ronda);
         //se reparte 2 cartas a cada jugador
-        for (int i = 0; i < cantidad_jugadores - cantidad_jugadores_eliminados; i++) {
+        for (int i = 0; i < juego.jugadores_en_ronda; i++) {
 
             (*(jugadores + i)).mano =  repartir(mazo, 2);
+            apuesta(&juego, jugadores+i, cartas_jugador);
         }
 
-        imprimir_mano(juego.mesa, juego.cantidad_cartas);
-        
-        break;
+ 
+
+        // break;
         //apuesta
     }
-    
-
-
-
 }
 
 void main () {
